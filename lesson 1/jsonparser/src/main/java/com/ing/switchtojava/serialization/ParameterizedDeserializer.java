6460a75d8e800b6e87cbac68e062a329d3bb1e01
mihaita-tinta <mihaita.tinta@gmail.com>
2018-10-02 16:41:17 +0300

    added homework

--- lesson 1/jsonparser/src/main/java/com/ing/switchtojava/serialization/ParameterizedDeserializer.java
+++ lesson 1/jsonparser/src/main/java/com/ing/switchtojava/serialization/ParameterizedDeserializer.java
@@ -3,5 +3,5 @@ package com.ing.switchtojava.serialization;
 public interface ParameterizedDeserializer {
 
     <T> T deserialize(String string, Class<T> clasz) throws DeserializationException;
-	// TODO 8 implement this class
+	// TODO 8 implement this in a class
 }
