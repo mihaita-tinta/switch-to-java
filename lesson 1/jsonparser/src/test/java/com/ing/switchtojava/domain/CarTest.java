b243e02e04b6cbbf686760b013dd4da98dcb232d
mihaita-tinta <mihaita.tinta@gmail.com>
2018-10-02 17:02:30 +0300

    added homework

---
+++ lesson 1/jsonparser/src/test/java/com/ing/switchtojava/domain/CarTest.java
@@ -0,0 +1,14 @@
+package com.ing.switchtojava.domain;
+
+import org.junit.Test;
+
+
+public class CarTest {
+
+    Car car;
+
+    @Test
+    public void test() {
+//        TODO 1 create instances of Car and assert fields values
+    }
+}
