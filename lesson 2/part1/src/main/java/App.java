f3416f88444cc47bc7e8790383dc3189e2cc92cb
Sandu Velea <sandu.velea@emag.ro>
2018-10-10 11:17:11 +0300

    create new files with uppercase content, move both files

--- lesson 2/part1/src/main/java/App.java
+++ lesson 2/part1/src/main/java/App.java
@@ -1,3 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
 
     /**
@@ -17,7 +26,27 @@ public class App {
                     "--------------------------------------------------------------\n");
         }
 
-        String input = "";//TODO 0  first argument is input directory
-        String output = "";// TODO 0 second argument is the output directory
+        String input = "/Users/sandu.velea/personal/java/fisiere/1";//TODO 0  first argument is input directory
+        String output = "/Users/sandu.velea/personal/java/fisiere/2";// TODO 0 second argument is the output directory
+        File inputDirectory = new File(input);
+        File outputDirectory = new File(output);
+        if (inputDirectory.isDirectory() && outputDirectory.exists() && outputDirectory.isDirectory()) {
+            File[] files = inputDirectory.listFiles();
+            if (files != null) {
+                Arrays.stream(files).forEach(file -> {
+                    try {
+                        Stream<String> stream = Files.lines(file.toPath());
+                        List<String> list = stream.map(String::toUpperCase).collect(Collectors.toList());
+                        String newExtension = file.getName().substring(0, file.getName().indexOf('.')) + ".transformed";
+                        File transformedFile = new File(inputDirectory.getPath().concat("/" + newExtension));
+                        Files.write(transformedFile.toPath(), list);
+                        Files.move(file.toPath(), Paths.get(outputDirectory.toPath() + "/" + file.getName()));
+                        Files.move(transformedFile.toPath(), Paths.get(outputDirectory.toPath() + "/" + transformedFile.getName()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
     }
 }
