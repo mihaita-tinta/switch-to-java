54c3dc52745c27efef8dfac06cdc2ed231a6e8ca
mihaita-tinta <mihaita.tinta@gmail.com>
2018-10-11 16:36:39 +0300

    tcp server

--- lesson 2/part1/src/main/java/game/SecretGame.java
+++ lesson 2/part1/src/main/java/game/SecretGame.java
@@ -61,6 +61,17 @@ public class SecretGame extends JComponent {
         // FIXME after working on the App class can you change this implementation so that
         // every ball instance is actually created only when a file is processed successfully?
         // can you write the filepath in the center of the ball?
+//        TODO does any of the principles below help you?
+//        Single responsibility principle[6]
+//        a class should have only a single responsibility (i.e. changes to only one part of the software's specification should be able to affect the specification of the class).
+//        Open/closed principle[7]
+//        "software entities … should be open for extension, but closed for modification."
+//        Liskov substitution principle[8]
+//        "objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program." See also design by contract.
+//                Interface segregation principle[9]
+//        "many client-specific interfaces are better than one general-purpose interface."[4]
+//        Dependency inversion principle[10]
+//        one should "depend upon abstractions, [not] concretions."[4]
     }
 }
            
