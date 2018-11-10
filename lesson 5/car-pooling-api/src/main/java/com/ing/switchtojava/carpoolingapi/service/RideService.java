b5c7dec0c967340b34fa7ab82e1494bf9691852e
sandu <veleassandu@gmail.com>
2018-11-10 19:32:50 +0200

    work in progress

--- lesson 5/car-pooling-api/src/main/java/com/ing/switchtojava/carpoolingapi/service/RideService.java
+++ lesson 5/car-pooling-api/src/main/java/com/ing/switchtojava/carpoolingapi/service/RideService.java
@@ -20,7 +20,6 @@ import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -56,7 +55,7 @@ public class RideService {
         return rideRepository.save(ride);
     }
 
-    private CarPosition getCarPositionsFromFile(Long rideId) throws JAXBException, IOException {
+    public CarPosition getCarPositionsFromFile(Long rideId) throws JAXBException, IOException {
         File file = new ClassPathResource("/routes/route0.gpx").getFile();
         JAXBContext jc = JAXBContext.newInstance(GpxType.class);
         GpxType route = ((JAXBElement<GpxType>) jc.createUnmarshaller().unmarshal(file)).getValue();
@@ -74,8 +73,8 @@ public class RideService {
         return carPosition;
     }
 
-    public SseEmitter track(Long rideId) throws JAXBException, IOException {
-        CarPosition carPosition = getCarPositionsFromFile(rideId);
+    public SseEmitter track(Long id) throws JAXBException, IOException {
+        CarPosition carPosition = getCarPositionsFromFile(id);
         SseEmitter emitter = new SseEmitter();
         ObjectMapper objectMapper = new ObjectMapper();
         Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
@@ -85,7 +84,7 @@ public class RideService {
                         String dataPosition = objectMapper.writeValueAsString(carPosition.next());
                         event.data(dataPosition + "\n")
                                 .id(String.valueOf(System.currentTimeMillis()))
-                                .name("Raw position for " + rideId);
+                                .name("Raw position for " + id);
                         emitter.send(event);
                     } catch (IOException e) {
                         e.printStackTrace();
